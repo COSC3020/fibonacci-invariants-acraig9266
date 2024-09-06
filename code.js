d59ae129e44b32b9e1b86513c2b5b918a1efc136
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-09-06 17:34:13 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,17 @@
 function fib(n) {
-    a = new Array(n);
+    let a = new Array(n);
     if (n == 0) {
         a[0] = 0;
         return a;
     }
-    a[0] = 0;
-    a[1] = 1;
+    a = [0, 1];
     if (n == 1) { return a; }
     for (i = 2; i <= n; i++) {
-        a[i] = fibHelp(i - 1) + fibHelp(i - 2);
+        a[i] = fibHelp(i)
     }
     return a;
 }
+
 function fibHelp(n) {
     if (n == 1) { return 1; }
     else if (n == 0) { return 0; }
